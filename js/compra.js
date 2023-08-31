3d4d71a9e189fae62692ac9bf90cac44e9e79b04
Arthur <130627221+ArthurYamori@users.noreply.github.com>
2023-08-31 09:09:41 -0300

    opa

--- js/compra.js
+++ js/compra.js
@@ -6,22 +6,24 @@ function itens(e){
 
     window.open('compra.html', '_self')
 
-    localStorage.setItem("valor", value)
+    localStorage.setItem("valor", e.value)
 
-    opa()
     
 
+
 }
 
 
-function opa(){
 
-    switch (localStorage.valor){
 
-        case 0:
-            imgMaior.setAttribute('src','/ASSETS/KILL THEM ALL TEE 1.webp')
+if (localStorage.getItem('valor') == 0) {
+    imgMaior.setAttribute('src','/ASSETS/KILL THEM ALL TEE 1.webp')
+} 
 
-            break
+if(localStorage.getItem('valor') == 1){
 
-    }
+    imgMaior.setAttribute('src','/ASSETS/Shape de skate/GIRLS END LOGO SKATEBOARD DECK/GIRLS END LOGO SKATEBOARD DECK 1.webp')
 }
+    
+
+
